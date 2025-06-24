9636e47fa81f0b59ff317f2a8e93298053eca4ec
Matyas <rusnak.matyas07@gmail.com>
2025-06-24 16:51:06 +0200

    Finished bank creating module casino

--- Casino.py
+++ Casino.py
@@ -2,4 +2,16 @@
 
 
 def main():
+    print("------------------")
+    print("Welcome to Casino!")
+    print("------------------")
+    print(" ")
+
+
+def casino():
     pass
+
+
+
+if __name__ is not "__main__":
+    casino()
