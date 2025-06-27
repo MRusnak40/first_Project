e91b973e52c5cd433f7605d78485f89f6e772cfe
Matyas <rusnak.matyas07@gmail.com>
2025-06-27 16:57:21 +0200

    Finished bank creating module casino

--- Casino.py
+++ Casino.py
@@ -8,16 +8,31 @@ def spin():
 
     return [random.choice(roulette) for _ in range(3)]
 
+def checkingList():
+    roulette = spin()
 
+    print("******************")
+    for x in roulette:
+        print(f"|-{x}-|",end=" ")
+
+    print(" ")
+    print("******************")
+
+    if roulette[0] == roulette[1]==roulette[2]:
+        print("You won!")
+    else:
+        print("You lost!")
 
 def casino():
     print("------------------")
     print("Welcome to Casino!")
     print("------------------")
     print(" ")
+    checkingList()
+
 
 
 if __name__ == "__main__":
     casino()
-    spin()
+
 
