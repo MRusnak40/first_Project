c1599e14a8c39fa1193901dd65eb8876e155e19f
Matyas <rusnak.matyas07@gmail.com>
2025-07-14 17:10:07 +0200

    classes.py

--- classes.py
+++ classes.py
@@ -26,18 +26,8 @@ class List_of_Workers:
         for list in List_of_Workers.list_of_Workers:
             print(list.ge)
 """
-class List_of_papers:
-    list_of_papers = list
 
-
-
-    @staticmethod
-    def get_all_papers_in_one(list_of_papers):
-        for list in list_of_papers:
-            print(list.get_all_papers())
-
-
-class Papers(Worker,List_of_papers):
+class Papers(Worker):
     def __init__(self, name, description, creator):
         self.name = name
         self.description = description
@@ -75,7 +65,7 @@ class Invoice(Papers):
         self.created_for = created_for
 
     def get_all_papers(self):
-        return f"Extract: {self.name} /*/ {self.description} /*/ {Worker.get_workers(self.creator)} /*/ {self.price:.2f}$ /*/ {self.created_for}"
+        return f"Invoice: {self.name} /*/ {self.description} /*/ {Worker.get_workers(self.creator)} /*/ {self.price:.2f}$ /*/ {Worker.get_workers(self.created_for)}"
 
 
 
@@ -89,11 +79,13 @@ list_of_Workers = [worker_1, worker_2, worker_3]
 for worker in list_of_Workers:
     print(worker.get_workers())
 
-print(random.choice(list_of_Workers))
+
 paper_1 = Contract("Paper 1", "Work contract", random.choice(list_of_Workers), "KAREL")
 paper_2 = Invoice("Paper 2", "Invoice for fridge", random.choice(list_of_Workers), 150, random.choice(list_of_Workers))
 paper_3 = Extract(name="Paper 3", description="Extract from archive", creator=random.choice(list_of_Workers), num_of_pages=20)
 # for x in range(10):
-List_of_papers.list_of_papers.append(paper_1, paper_2,paper_3)
 
+list=[paper_1, paper_2, paper_3]
 
+for paper in list:
+    print(paper.get_all_papers())
