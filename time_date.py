6775a528b2eecd5e4b6761136d1b403c5e38f42c
Matyas <rusnak.matyas07@gmail.com>
2025-07-17 21:25:05 +0200

    time

--- time_date.py
+++ time_date.py
@@ -1,1 +1,12 @@
 import datetime
+
+date = datetime.date(2025,1,2)
+
+
+
+time= datetime.time
+time = datetime.time(hour=1,minute=1,second=1)
+now = datetime.datetime.now()
+
+now=now.strftime("%m/%d/%Y, %H:%M:%S")
+print(now)
