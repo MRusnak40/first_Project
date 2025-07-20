3f8de942c581c4e797f6ec384b786df115f1d3b4
Matyas <rusnak.matyas07@gmail.com>
2025-07-20 17:02:21 +0200

    time alarm_clock.py

--- alarm_clock.py
+++ alarm_clock.py
@@ -6,6 +6,7 @@ import pygame
 
 def set_alarm_clock(alarm_clock):
     print("Alarm Clock set for " + alarm_clock)
+    sound_file="Scooter x Blasterjaxx x CERES - Heart Attack.mp3"
 
 if __name__ == '__main__':
     alarm_time=input("Enter alarm time (HH:MM:SS):")
