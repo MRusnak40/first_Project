6f5d5a4b9b1c8e7ac8504e7859de23291c678a66
Matyas <rusnak.matyas07@gmail.com>
2025-07-20 18:12:42 +0200

    time alarm_clock.py pygame module

--- alarm_clock.py
+++ alarm_clock.py
@@ -3,11 +3,32 @@ import time
 import pygame
 
 
-
 def set_alarm_clock(alarm_clock):
     print("Alarm Clock set for " + alarm_clock)
-    sound_file="Scooter x Blasterjaxx x CERES - Heart Attack.mp3"
+    sound_file = "Scooter x Blasterjaxx x CERES - Heart Attack.mp3"
+    is_rinning = True
+
+    while is_rinning:
+        curent_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(curent_time)
+        if curent_time == alarm_clock:
+            print("+++++++++++++++++++")
+            print("WAKE UP")
+            print(" ")
+            print(" ")
+            print("I LIKE IT LOUD")
+            print("+++++++++++++++++++")
+
+            pygame.mixer.init()
+            pygame.mixer.music.load(sound_file)
+            pygame.mixer.music.play()
+
+            while (pygame.mixer.music.get_busy()):
+                time.sleep(1)
+
+        time.sleep(1)
+
 
 if __name__ == '__main__':
-    alarm_time=input("Enter alarm time (HH:MM:SS):")
+    alarm_time = input("Enter alarm time (HH:MM:SS):")
     set_alarm_clock(alarm_time)
