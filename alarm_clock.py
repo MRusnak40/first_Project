92e35b23c16bb47583b4398a50c0df4f8f8fbdbb
Matyas <rusnak.matyas07@gmail.com>
2025-07-21 16:26:34 +0200

    time alarm_clock.py pygame module almost finished

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,16 +1,19 @@
 import datetime
 import time
+from logging import raiseExceptions
+
 import pygame
 
 
 def set_alarm_clock(alarm_clock):
-    print("Alarm Clock set for " + alarm_clock)
+    print(f"Alarm Clock set for {alarm_clock}")
     sound_file = "Scooter x Blasterjaxx x CERES - Heart Attack.mp3"
     is_rinning = True
 
     while is_rinning:
         curent_time = datetime.datetime.now().strftime("%H:%M:%S")
         print(curent_time)
+
         if curent_time == alarm_clock:
             print("+++++++++++++++++++")
             print("WAKE UP")
@@ -24,11 +27,55 @@ def set_alarm_clock(alarm_clock):
             pygame.mixer.music.play()
 
             while (pygame.mixer.music.get_busy()):
-                time.sleep(1)
+                if input("IM WANKEN press q >>>").lower() == "q":
+                    pygame.mixer.music.stop()
+                    print(" ")
 
         time.sleep(1)
 
 
+def alarm_clock(hour, minute, seconds):
+    if (int(hour) >= 0 and int(hour) <= 23) and (int(minute) >= 0 and int(minute) <= 59) and (int(seconds) >= 0 and int(seconds) <= 59):
+        alarm_time_setter = f"{str(hour)}:{str(minute)}:{str(seconds)}"
+
+
+        return alarm_time_setter
+    else:
+        raise Exception("Invalid alarm clock")
+
+    """
+    
+    elif hour is None:
+        alarm_time_setter = f"{00}:{minute}:{seconds}"
+        print(alarm_time_setter)
+
+        return alarm_time_setter
+    elif minute is None:
+        alarm_time_setter = f"{hour}:{00}:{seconds}"
+        print(alarm_time_setter)
+
+        return alarm_time_setter
+    elif seconds is None:
+        alarm_time_setter = f"{hour}:{minute}:{00}"
+        print(alarm_time_setter)
+        return alarm_time_setter 
+        
+    """
+
+
 if __name__ == '__main__':
-    alarm_time = input("Enter alarm time (HH:MM:SS):")
+    print("Alarm Clock SET")
+    print("In  >> HH:MM:SS <<")
+    while True:
+        try:
+            alarm_time = alarm_clock(input("HOURS:"), input("MINUTES:"), input("SECONDS:"))
+            if alarm_time is not None:
+                break
+
+        except Exception:
+            print("ERROR---->Wrong Input")
+            print("TRY AGAIN")
+            print("  ")
+            continue
+
     set_alarm_clock(alarm_time)
